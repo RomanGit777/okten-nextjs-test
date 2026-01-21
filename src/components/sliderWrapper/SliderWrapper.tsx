1e6fdf769dba637a819680740f335b6ec12bb6dc
RomanGit777 <rsavenko91@gmail.com>
2026-01-21 22:12:07 +0100

    scroll logic

--- src/components/sliderWrapper/SliderWrapper.tsx
+++ src/components/sliderWrapper/SliderWrapper.tsx
@@ -1,13 +1,13 @@
 'use client';
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import style from './sliderWrapper.module.css';
 
 interface SliderWrapperProps {
     children: React.ReactNode;
     Arrows: React.ComponentType<{
-        scrollRight: () => void;
-        scrollLeft: () => void;
+        scroll: (direction: "right" | "left") => void;
         canGoBack: boolean;
+        canGoForward: boolean;
     }>;
 }
 
@@ -15,25 +15,43 @@ export const SliderWrapper = ({Arrows, children}: SliderWrapperProps) => {
 
     const sliderRef = useRef<HTMLDivElement>(null);
     const [canGoBack, setCanGoBack] = useState(false);
+    const [canGoForward, setCanGoForward] = useState(true)
     const [show, setShow] = useState(false);
 
-
-    const scrollRight = () => {
+    useEffect(() => {
         const slider = sliderRef.current;
         if (!slider) return;
 
-        slider.scrollBy({left: 1100, behavior: "smooth"});
-        setCanGoBack(true);
-    }
+        const handleScroll = () => {
+            const maxScroll = slider.scrollWidth - slider.clientWidth;
 
-    const scrollLeft = () => {
+            setCanGoBack(slider.scrollLeft > 0);
+            setCanGoForward(slider.scrollLeft + 5 < maxScroll);
+        };
+
+        slider.addEventListener("scroll", handleScroll);
+
+        handleScroll();
+
+        return () => slider.removeEventListener("scroll", handleScroll);
+    }, [])
+
+    const scrollByStep = (direction: "right" | "left") => {
         const slider = sliderRef.current;
         if (!slider) return;
 
-        slider.scrollBy({left: -1100, behavior: "smooth"});
-        if (slider.scrollLeft - 1100 <= 0) {
-            setCanGoBack(false);
-        }
+        const step = 1100;
+        const maxScroll = slider.scrollWidth - slider.clientWidth;
+
+        const nextScrollLeft =
+            direction === "right"
+        ? Math.min(slider.scrollLeft + step, maxScroll)
+        : Math.max(slider.scrollLeft - step, 0);
+
+        slider.scrollTo({
+            left: nextScrollLeft,
+            behavior: "smooth",
+        })
     }
 
     return (
@@ -41,9 +59,10 @@ export const SliderWrapper = ({Arrows, children}: SliderWrapperProps) => {
         onMouseEnter={() => setShow(true)}
         onMouseLeave={() => setShow(false)}>
             {show && ( <Arrows
-                scrollRight={scrollRight}
-                scrollLeft={scrollLeft}
-                canGoBack={canGoBack} /> )}
+                scroll={scrollByStep}
+                canGoBack={canGoBack}
+                canGoForward={canGoForward}
+            /> )}
             <div ref={sliderRef} className={style.slider}>
                 {children}
             </div>
