6fa083e8121ca80f411485c7203e3ed4ee5164e0
RomanGit777 <rsavenko91@gmail.com>
2026-01-24 21:40:03 +0100

    added service for movies

--- src/api/getMovies.ts
+++ src/api/getMovies.ts
@@ -3,10 +3,14 @@ import {IPopularTvBase} from "@/models/IPopularTvBase";
 import {ITVDetails} from "@/models/ITvDetails";
 
 export const getPopularTv = async (time_window: "day" | "week"): Promise<IPopularTvBase> => {
-    const result = await axios.get(`https://api.themoviedb.org/3/trending/tv/${time_window}?api_key=${process.env.API_KEY}`)
-    return result.data
+    const {data} = await axios.get(`https://api.themoviedb.org/3/trending/tv/${time_window}?api_key=${process.env.API_KEY}`)
+    return data
 }
 export const getPopularTvById = async (id: string): Promise<ITVDetails> => {
     const {data} = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.API_KEY}`)
+    return data
+}
+export const getPopularMovies = async (time_window: "day" | "week"): Promise<IPopularTvBase> => {
+    const {data} = await axios.get(`https://api.themoviedb.org/3/trending/movie/${time_window}?api_key=${process.env.API_KEY}`)
     return data
 }
