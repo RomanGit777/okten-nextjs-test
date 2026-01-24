adc7f0e07897a6d82e1a0783d64878fb8d988071
RomanGit777 <rsavenko91@gmail.com>
2026-01-24 13:32:32 +0100

    added rating on card

--- src/components/movieInfo/MovieInfo.tsx
+++ src/components/movieInfo/MovieInfo.tsx
@@ -14,6 +14,10 @@ export const MovieInfo = ({tv, shortOverview}: movieInfoProps) => {
             <div className={style.text}>
                 <h2 className={style.title}>{tv.name}</h2>
                 <p className={style.desc}>{shortOverview}</p>
+                <p className={style.rating}
+                   style={{backgroundColor: tv.vote_average >= 7 ? "green" : "red"}}>
+                    {tv.vote_average.toFixed(1)}
+                </p>
             </div>
         </div>
     );
