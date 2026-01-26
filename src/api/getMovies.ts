86e6d65cc4135912acceaee50adf45643c7e3e17
RomanGit777 <rsavenko91@gmail.com>
2026-01-26 20:39:01 +0100

    added/changed models + added request for moviesById

--- src/api/getMovies.ts
+++ src/api/getMovies.ts
@@ -1,17 +1,23 @@
 import axios from "axios";
-import {ITVDetails} from "@/models/ITvDetails";
+import {ITvDetails} from "@/models/ITvDetails";
 import {IPopularBaseTv} from "@/models/IPopularBaseTv";
 import {IPopularBaseMovies} from "@/models/IPopularBaseMovies";
+import {IMovieDetails} from "@/models/IMovieDetails";
 
 export const getPopularTv = async (time_window: "day" | "week"): Promise<IPopularBaseTv> => {
     const {data} = await axios.get(`https://api.themoviedb.org/3/trending/tv/${time_window}?api_key=${process.env.API_KEY}`)
     return data
 }
-export const getPopularTvById = async (id: string): Promise<ITVDetails> => {
+export const getPopularTvById = async (id: string): Promise<ITvDetails> => {
     const {data} = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.API_KEY}`)
     return data
 }
+
 export const getPopularMovies = async (time_window: "day" | "week"): Promise<IPopularBaseMovies> => {
     const {data} = await axios.get(`https://api.themoviedb.org/3/trending/movie/${time_window}?api_key=${process.env.API_KEY}`)
     return data
+}
+export const getPopularMoviesById = async (id: string): Promise<IMovieDetails> => {
+    const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}`)
+    return data;
 }
---
+++ src/models/IMovieDetails.ts
@@ -0,0 +1,51 @@
+export interface IMovieDetails {
+    original_language: string;
+    imdb_id: string;
+    video: boolean;
+    title: string;
+    backdrop_path: string;
+    revenue: number;
+    genres: Array<genresItem>;
+    popularity: number;
+    production_countries: Array<production_countriesItem>;
+    id: number;
+    vote_count: number;
+    budget: number;
+    overview: string;
+    original_title: string;
+    runtime: number;
+    poster_path: string;
+    spoken_languages: Array<spoken_languagesItem>;
+    production_companies: Array<production_companiesItem>;
+    release_date: string;
+    vote_average: number;
+    belongs_to_collection: string;
+    tagline: string;
+    adult: boolean;
+    homepage: string;
+    status: string;
+}
+export interface spoken_languagesItem {
+	name: string;
+	iso_639_1: string;
+	english_name: string;
+}
+
+export interface production_companiesItem {
+	logo_path: string;
+	name: string;
+	id: number;
+	origin_country: string;
+}
+
+export interface genresItem {
+	name: string;
+	id: number;
+}
+
+export interface production_countriesItem {
+	iso_3166_1: string;
+	name: string;
+}
+
+
