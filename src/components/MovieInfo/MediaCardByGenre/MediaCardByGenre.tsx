3e3007ba4ea808f2397af7588b3fea5e83d02505
RomanGit777 <rsavenko91@gmail.com>
2026-01-28 21:49:27 +0100

    html of card

--- src/components/MovieInfo/MediaCardByGenre/MediaCardByGenre.tsx
+++ src/components/MovieInfo/MediaCardByGenre/MediaCardByGenre.tsx
@@ -1,6 +1,9 @@
 import style from './style.module.css'
 import {IPopularTv} from "@/models/IPopularTv";
 import {IPopularMovies} from "@/models/IPopularMovies";
+import Link from "next/link";
+import {PosterPreview} from "@/components/posterPreview/PosterPreview";
+import {GENRE_MAP} from "@/constants/genres";
 
 type Media = IPopularTv | IPopularMovies;
 
@@ -11,7 +14,24 @@ interface MediaCardByGenreProps {
 }
 
 export const MediaCardByGenre = ({item, shortOverview, type}: MediaCardByGenreProps) => {
+
+    const title = "name" in item ? item.name : item.title;
+
     return (
-        <div>F</div>
+        <Link href={`/${type}/` + item.id.toString()}>
+            <div className={style.card} role={"button"}>
+                <PosterPreview title={title} posterPath={item.poster_path} className={style.cardImg}/>
+
+                <div className={style.text}>
+                    <h2 className={style.title}>{title}</h2>
+                    <p>{shortOverview}</p>
+                    <div className={style.downPart}>
+                        <p className={style.rating} style={{backgroundColor: item.vote_average >= 7 ? "green" : "red"}}>
+                            {item.vote_average.toFixed(1)}</p>
+                        <p className={style.genre}>{item.genre_ids.map(id => GENRE_MAP[id]).join(", ")}</p>
+                    </div>
+                </div>
+            </div>
+        </Link>
     );
 };
