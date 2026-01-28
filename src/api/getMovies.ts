5a830df82248273f758968bbca3a1391383ae52b
RomanGit777 <rsavenko91@gmail.com>
2026-01-28 13:19:56 +0100

    added requests for genre id

--- src/api/getMovies.ts
+++ src/api/getMovies.ts
@@ -12,6 +12,10 @@ export const getPopularTvById = async (id: string): Promise<ITvDetails> => {
     const {data} = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.API_KEY}`)
     return data
 }
+export const getTvMoviesByGenre = async (genreId: string): Promise<IPopularBaseTv> => {
+    const {data} = await axios.get(`https://api.themoviedb.org/3/discover/tv??with_genres=${genreId}&api_key=${process.env.API_KEY}`)
+    return data;
+}
 
 export const getPopularMovies = async (time_window: "day" | "week"): Promise<IPopularBaseMovies> => {
     const {data} = await axios.get(`https://api.themoviedb.org/3/trending/movie/${time_window}?api_key=${process.env.API_KEY}`)
@@ -19,5 +23,9 @@ export const getPopularMovies = async (time_window: "day" | "week"): Promise<IPo
 }
 export const getPopularMoviesById = async (id: string): Promise<IMovieDetails> => {
     const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`)
+    return data;
+}
+export const getMoviesByGenre = async (genreId: string): Promise<IPopularBaseTv> => {
+    const {data} = await axios.get(`https://api.themoviedb.org/3/discover/movie??with_genres=${genreId}&api_key=${process.env.API_KEY}`)
     return data;
 }
--- src/components/genresSection/GenresSection.tsx
+++ src/components/genresSection/GenresSection.tsx
@@ -1,14 +1,17 @@
 'use client';
 import style from './style.module.css'
 import {GENRE_MAP} from "@/constants/genres";
+import Link from "next/link";
 
 export const GenresSection = () => {
 
  return (
+
         <div className={style.wrapper}>
             <h1 className={style.title}>Genres</h1>
                     <ul className={style.genresContainer}>
-                        {Object.entries(GENRE_MAP).map(([id, name]) => <li key={id} className={style.genresList}>{name}</li>)}
+                        {Object.entries(GENRE_MAP).map(([id, name]) =>
+                            <Link href={`/genres/${id}`} key={id}><li className={style.genresList}>{name}</li></Link> )}
                     </ul>
         </div>
     );
