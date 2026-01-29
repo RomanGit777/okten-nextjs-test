3a9ed9cf91257c1cfb974755034df1937a6a4486
RomanGit777 <rsavenko91@gmail.com>
2026-01-29 16:06:44 +0100

    fixed header to scroll it, added more margin

--- src/containers/MoviesByGenrePage/MoviesByGenrePage.tsx
+++ src/containers/MoviesByGenrePage/MoviesByGenrePage.tsx
@@ -11,7 +11,7 @@ export const MoviesByGenrePage = async ({id} : MoviesByGenrePageProps) => {
         getTvByGenre(id)
     ])
     return (
-        <div style={{paddingBottom: "20px"}}>
+        <div style={{paddingBottom: "20px", paddingTop: "60px"}}>
             {movies.length > 0 && <MoviesByGenreList movies={movies}/>}
             {tv.length > 0 && <TvByGenreList tv={tv}/>}
         </div>
