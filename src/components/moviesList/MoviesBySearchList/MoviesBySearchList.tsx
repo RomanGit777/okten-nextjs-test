50926491178a984aaaa2cb2aa97e00e9818bca5b
RomanGit777 <rsavenko91@gmail.com>
2026-02-06 14:41:34 +0100

    added fail page in search

--- src/components/moviesList/MoviesBySearchList/MoviesBySearchList.tsx
+++ src/components/moviesList/MoviesBySearchList/MoviesBySearchList.tsx
@@ -7,7 +7,9 @@ interface MoviesBySearchListProps {
 }
 
 export const MoviesBySearchList = ({movies} : MoviesBySearchListProps) => {
-
+    if (!movies.length) return <div className={style.failPage}>
+        <p>We could not find anything</p>
+        <p>Are there any grammatical errors in the request?</p></div> ;
     return (
         <div className={style.gridContainer}>
             {movies.map(m => <MoviesBySearchListCard movie={m} key={m.id}/>)}
